a353ee2870b2c1e55888c870a336680fa38decef
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-06-23 00:05:01 -0400

    some of exercise 2

---
+++ src/exercises/ArrayPractice.java
@@ -0,0 +1,31 @@
+package exercises;
+
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] someNumbers =  { 1, 1, 2, 3, 5, 8 };
+        for(int i : someNumbers) {
+            if (i%2 != 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+        String storyString = "I would not, could not, in a box. " +
+                "I would not, could not with a fox. " +
+                "I will not eat them in a house. " +
+                "I will not eat them with a mouse.";
+        //String[] storyArray = {};
+        //storyString = storyString.replace(',',' ').replace('.',' ');
+        String[] storyArray = storyString.split("\\.");
+        for (String i : storyArray) {
+            //System.out.println(Arrays.toString(storyArray));
+            System.out.println(i.trim() + '.');
+        }
+    }
+}
+
+

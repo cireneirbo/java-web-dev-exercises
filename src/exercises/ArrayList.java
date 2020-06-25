e7ed17a04c997b8adc0271ca95f4ed7089dea511
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-06-24 21:43:00 -0400

    switching projects commit

--- src/exercises/ArrayList.java
+++ src/exercises/ArrayList.java
@@ -2,3 +2,17 @@ package exercises;
 
 public class ArrayList {
 }
+
+
+/*
+Write a static method to find the sum of all the even numbers in an ArrayList.
+Within main, create a list with at least 10 integers and call your method on the list.
+Write a static method to print out each word in a list that has exactly 5 letters.
+Modify your code to prompt the user to enter the word length for the search.
+
+BONUS: Instead of creating our own list of words,
+what if we want to use the string from the Array Practice section?
+
+Search “Java convert String to ArrayList” online to see how to split a
+string into the more flexible ArrayList collection
+*/
